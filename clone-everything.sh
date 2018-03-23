f0ce116bb5e46f51dc6c405b83e51d08f73f7f30
Aaron Faanes <dafrito@gmail.com>
2018-03-22 23:58:00 -0500

    Fix some cloning problems

--- clone-everything.sh
+++ clone-everything.sh
@@ -4,15 +4,14 @@ ln -s tools/clean-everything.sh
 ln -s tools/deploy-everything.sh
 ln -s tools/rebuild-everything.sh
 mkdir -p public_html rpm
-git clone https://github.com/dafrito/mod_rainback
-cd mod_rainback && autoreconf -i && cd ..
-git clone https://github.com/dafrito/rainback-doc doc
+test -d mod_rainback || git clone https://github.com/dafrito/mod_rainback
+test -d doc || git clone https://github.com/dafrito/rainback-doc doc
 cd doc && autoreconf -i && cd ..
-git clone https://github.com/dafrito/graph-js
+test -d graph-js || git clone https://github.com/dafrito/graph-js
 cd graph-js && autoreconf -i && cd ..
-git clone https://github.com/dafrito/libparsegraph
+test -d libparsegraph || git clone https://github.com/dafrito/libparsegraph
 cd libparsegraph && autoreconf -i && cd ..
-git clone https://github.com/dafrito/marla
+test -d marla || git clone https://github.com/dafrito/marla
 cd marla && autoreconf -i && cd ..
-git clone https://github.com/dafrito/server
+test -d server || git clone https://github.com/dafrito/rainback-server-httpd server
 cd server && make && cd ..
