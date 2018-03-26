8a361bd8e070b0723c38da12876ac7e9f245f431
Aaron Faanes <dafrito@gmail.com>
2018-03-26 18:32:58 -0500

    Add more deps

--- install-fedora-deps.sh
+++ install-fedora-deps.sh
@@ -1,1 +1,1 @@
-dnf install openssl-devel httpd-devel automake autoconf libtool
+dnf install openssl-devel httpd-devel automake autoconf libtool cairo-devel libdrm-devel mesa-libgbm-devel mesa-libEGL-devel mesa-libGL-devel libudev-devel libinput-devel libevdev-devel ncurses-devel
