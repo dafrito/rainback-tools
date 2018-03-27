d95ec90648bab5e536e262756661bed8d0d023c3
Aaron Faanes <dafrito@gmail.com>
2018-03-27 01:14:25 -0500

    Add more deps

--- install-fedora-deps.sh
+++ install-fedora-deps.sh
@@ -1,1 +1,1 @@
-dnf install openssl-devel httpd-devel automake autoconf libtool cairo-devel libdrm-devel mesa-libgbm-devel mesa-libEGL-devel mesa-libGL-devel libudev-devel libinput-devel libevdev-devel ncurses-devel qt5-qtbase-devel apr-util-sqlite sqlite-devel
+dnf install openssl-devel httpd-devel automake autoconf libtool cairo-devel libdrm-devel mesa-libgbm-devel mesa-libEGL-devel mesa-libGL-devel libudev-devel libinput-devel libevdev-devel ncurses-devel qt5-qtbase-devel apr-util-sqlite sqlite-devel xsel tmux
